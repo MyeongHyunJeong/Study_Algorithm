3b9fa8399aa1584ac9691f9a6aee6dfa717e2946
MyeongHyunJeong <mh_jeong93@naver.com>
2020-04-29 18:45:26 +0900

    Solved Baekjoon 17837 Finally!

--- Day42/src/Main17837.java
+++ Day42/src/Main17837.java
@@ -1,24 +1,23 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main17837 {
-	
+
 	public static int n;
 	public static int k;
 	public static int[][] map;
 	public static Mal[] mal;
 	public static int[][] direction = {{0,1},{0,-1},{-1,0},{1,0}};
-	public static Deque<Mal>[][] state;
+	public static Stack<Mal>[][] state;
 
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("input17837.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		n = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
 		map = new int[n][n];
@@ -28,12 +27,12 @@ public class Main17837 {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		printMap();
-		
-		state = new Deque[n][n];
+//		printMap();
+
+		state = new Stack[n][n];
 		for(int i=0; i<n; i++) {
 			for(int j=0; j<n; j++) {
-				state[i][j] = new LinkedList<Mal>();
+				state[i][j] = new Stack<Mal>();
 			}
 		}
 		mal = new Mal[k];
@@ -47,44 +46,119 @@ public class Main17837 {
 			state[x][y].add(mal[i]);
 		}
 		
+//		state[0][0].add(new Mal(3,0,0,1));
+//		state[0][0].add(new Mal(3,0,0,2));
+//		for(Mal m : state[0][0]) {
+//			System.out.println(m.num);
+//		}
+//		int ad = state[0][0].size();
+//		for(int i=0; i<ad; i++) {
+//			System.out.println(state[0][0].pop().num);
+//		}
+		
 		move();
-		
-		
+
 		br.close();
 	}
-	
+
 	public static void move() {
 		int count = 0;
 		boolean flag = false;
-		
+
+		Stack<Mal> temp = new Stack<Mal>();
 		Mal curmal;
 		int x, y, dir, nx, ny, ndir;
 		while(true) {
-			
-			
+
+
 			for(int i=0; i<k; i++) {	//움직이기
 				curmal = mal[i];
 				x = curmal.x;
 				y = curmal.y;
 				dir = curmal.dir;
-				if(state[x][y].size()==4) {
-					flag = true;
-					break;
+				nx = x + direction[dir][0];
+				ny = y + direction[dir][1];
+				if(nx>-1 && ny>-1 && nx<n && ny<n && map[nx][ny]!=2) {
+					if(map[nx][ny]==0) {
+						Mal m;
+						while(!state[x][y].isEmpty()) {
+							m = state[x][y].pop();
+							m.x = nx;
+							m.y = ny;
+							temp.add(m);
+							if(m.num==i) break;
+						}
+						while(!temp.isEmpty()) {
+							state[nx][ny].add(temp.pop());
+						}
+					}else if(map[nx][ny]==1) {
+						Mal m;
+						while(!state[x][y].isEmpty()) {
+							m = state[x][y].pop();
+							m.x = nx;
+							m.y = ny;
+							state[nx][ny].add(m);
+							if(m.num==i) break;
+						}
+					}
 				}else {
-					nx = x + direction[dir][0];
-					ny = y + direction[dir][1];
+					if(curmal.dir==0) {
+						curmal.dir = 1;
+					}else if(curmal.dir==1) {
+						curmal.dir = 0;
+					}else if(curmal.dir==2) {
+						curmal.dir = 3;
+					}else {
+						curmal.dir = 2;
+					}
+					nx = x + direction[curmal.dir][0];
+					ny = y + direction[curmal.dir][1];
+					if(nx>-1 && ny>-1 && nx<n && ny<n && map[nx][ny]!=2) {
+						if(map[nx][ny]==0) {
+							Mal m;
+							while(!state[x][y].isEmpty()) {
+								m = state[x][y].pop();
+								m.x = nx;
+								m.y = ny;
+								temp.add(m);
+								if(m.num==i) break;
+							}
+							while(!temp.isEmpty()) {
+								state[nx][ny].add(temp.pop());
+							}
+						}else if(map[nx][ny]==1) {
+							Mal m;
+							while(!state[x][y].isEmpty()) {
+								m = state[x][y].pop();
+								m.x = nx;
+								m.y = ny;
+								state[nx][ny].add(m);
+								if(m.num==i) break;
+							}
+						}
+					}else {
+						nx = x;
+						ny = y;
+					}
+				}
+//				System.out.println("C : " + count + " Num : " + curmal.num + " NX : " + nx + " NY :" + ny);
+//				for(Mal m : state[nx][ny]) {
+//					System.out.println(m.num + " " + m.x  + " " + m.y  + " " + m.dir);
+//				}
+				if(state[nx][ny].size()>=4) {
+					flag=true;
+					break;
 				}
 			}
-			
-			
-			
-			
+
+			count++;
 			if(flag) break;
 			if(count>1000) break;
-			count++;
 		}
+		if(count>1000) System.out.println(-1);
+		else System.out.println(count);
 	}
-	
+
 	public static void printMap() {
 		for(int i=0; i<n; i++) {
 			for(int j=0; j<n; j++) {
@@ -92,7 +166,7 @@ public class Main17837 {
 			}System.out.println();
 		}System.out.println();
 	}
-	
+
 	public static class Mal {
 		int x;
 		int y;
